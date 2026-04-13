37cc5b2abbe0cf09ae7dce239c94b64de154f2fa
Petri-Hub <fernando.petri01@gmail.com>
2026-04-12 23:25:52 -0300

    chore: remove unused code ans assets

--- jarvis-api/src/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export { AuthModule } from './auth.module';
-export { JwtAuthGuard } from './guards/jwt-auth.guard';
-export { JwtStrategy } from './strategies/jwt.strategy';
-export type { AuthenticatedUser } from './strategies/jwt.strategy';
